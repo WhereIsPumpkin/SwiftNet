631d28be8af6fde2f469752e8f50bfd625574479
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-01-23 13:15:06 +0400

    feat: add request header application/json

--- Sources/NetSwift/NetSwift.swift
+++ Sources/NetSwift/NetSwift.swift
@@ -3,22 +3,25 @@ import Foundation
 @available(iOS 13.0.0, *)
 public final class NetworkManager {
     public static let shared = NetworkManager()
-
+    
     private init() {}
-
+    
     public enum NetworkError: Error {
         case noData
     }
-
+    
     public func fetchDecodableData<T: Decodable>(from url: URL, responseType: T.Type) async throws -> T {
-        let (data, _) = try await URLSession.shared.data(from: url)
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
         guard !data.isEmpty else {
             throw NetworkError.noData
         }
         let decodedData = try JSONDecoder().decode(T.self, from: data)
         return decodedData
     }
-
+    
     public func postData<T: Encodable>(to url: URL, body: T) async throws -> (Data, URLResponse) {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
